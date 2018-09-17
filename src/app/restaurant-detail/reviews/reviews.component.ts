95ab61daab762e46dce21278a2f1b241a7977419
Lucas Nessi <lucasnessi@gmail.com>
2018-09-17 15:15:09 -0300

    47

---
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Injectable } from '@angular/core';
+
+@Injectable()
+@Component({
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -24,4 +24,10 @@ export class RestaurantsService {
     .map(response => response.json())
     .catch(ErrorHandler.handleError);
     }
+
+    restaurantById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
+    }
 }
